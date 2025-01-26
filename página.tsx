1a8c11b1c13b9fac1994a75ab2da89e33d5d4004
Thayse-Dias <156327575+Thayse-Dias@users.noreply.github.com>
2025-01-26 11:39:17 -0300

    Add files via upload

--- página.tsx
+++ página.tsx
@@ -1,8 +1,8 @@
 import Image from "next/image"
 import Link from "next/link"
-import LoginForm from "@/components/LoginForm"
+import RegisterForm from "@/components/RegisterForm"
 
-export default function Home() {
+export default function Register() {
   return (
     <div className="flex h-screen">
       <div className="w-1/2 flex flex-col justify-center items-center bg-white">
@@ -13,13 +13,13 @@ export default function Home() {
         </div>
       </div>
       <div className="w-1/2 flex justify-center items-center">
-        <div className="login-frame w-full max-w-sm">
-          <h3 className="text-2xl font-bold mb-6 text-center">Faça seu Login</h3>
-          <LoginForm />
+        <div className="w-full max-w-md">
+          <h2 className="text-2xl font-bold mb-6 text-center">Cadastro de Usuário</h2>
+          <RegisterForm />
           <p className="mt-4 text-sm text-center">
-            Não possui conta?{" "}
-            <Link href="/register" className="text-blue-500 hover:underline">
-              Cadastre-se
+            Já tem uma conta?{" "}
+            <Link href="/" className="text-blue-500 hover:underline">
+              Faça login aqui
             </Link>
           </p>
         </div>
